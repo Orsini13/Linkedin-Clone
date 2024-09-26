9ff738b373000752dd5a44fc9aeb93c58071ccfd
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2024-09-26 01:07:25 +0100

    first comment

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import './App.css';
+import Login from "./Components/Login"
+
+
+function App() {
+  return (
+    <div className="App">
+       <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Login/>
+          </Route>
+        </Switch>
+       </Router>
+      
+    </div>
+  );
+}
+
+export default App;
+
+
